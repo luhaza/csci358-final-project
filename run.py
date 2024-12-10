0f01f64e006cdd829022c449ceb0d77dae7c3a95
lukezann <luke.zanuck@gmail.com>
2024-12-09 23:13:57 -0500

    fixed midseason races

--- run.py
+++ run.py
@@ -4,13 +4,13 @@ import os
 plan_length = random.randint(7, 365)
 rest_days = random.randint(0, 4)
 baseline_fitness = random.randint(1, 100)
+
 if plan_length >= 60:
-    # TODO: fix this
-    max_races = min(plan_length // 14, 2)
+    max_races = plan_length // 14 - 1
 else:
     max_races = 1
     
-num_races = random.randint(0, max_races)
+num_races = random.randint(1, max_races)
 
 print(f"Creating plan with plan_length={plan_length} rest_days={rest_days} baseline_fitness={baseline_fitness} num_races={num_races}")
 os.system(f"python create_lp.py {plan_length} {rest_days} {baseline_fitness} {num_races}")
